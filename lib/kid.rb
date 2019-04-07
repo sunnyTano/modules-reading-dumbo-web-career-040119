686fd23b115d7e24bad7a3ada56957f55cba7dc4
Susan Tan <ssusantan9@gmail.com>
2019-04-07 02:11:37 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,7 +3,12 @@ require_relative './class_methods_module.rb'
 
 class Kid
   
-  class Dancer
-end
+include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
   
 end
